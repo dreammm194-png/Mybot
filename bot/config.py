cb8fa5ac0e626b216cb5a099bd23aa545d5ffcbc
dreammm194-png <dreammm194@gmail.com>
2026-03-07 03:47:59 +0300

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,7 @@
+import os
+
+# Секреты для бота и API
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "")
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN", "")
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY", "")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY", "")
